9005093c06bc14db83fac53adb91689836610709
hemraj kumawat <hemrajkumawat68005@gmail.com>
2025-06-23 16:34:42 +0300

    Updated project

--- app/page.js
+++ app/page.js
@@ -799,24 +799,7 @@ export default function Home() {
             </div>
           </div>
           {/* Contact End */}
-          {/* Footer Start */}
-          <div className="footer">
-            <div className="content-inner">
-              <div className="row align-items-center">
-                <div className="col-md-6">
-                  <p>
-                    © Copyright <a href="https://htmlcodex.com">HTML Codex</a>. All
-                    Rights Reserved
-                  </p>
-                </div>
-                <div className="col-md-6">
-                  <p>
-                    Powered by <a href="https://htmlcodex.com">HTML Codex</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+    
           {/* Footer Start */}
         </div>
       </div>
